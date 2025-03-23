87d38a39fa7899b103de2bb1aafd3af0a675d419
Radu Gafita <radugaf@gmail.com>
2025-03-23 22:38:24 +0200

    Added basic layout for the POS

--- Scenes/POSTerminal/POSMain.cs
+++ Scenes/POSTerminal/POSMain.cs
@@ -3,9 +3,24 @@ using System;
 
 public partial class POSMain : Control
 {
+
+    private Button _exitButton;
     public override void _Ready()
     {
-        GD.Print("POSMain: Initializing");
+        // Initialize logger
+        var logger = GetNode<Node>("/root/Logger");
+        logger.Call("info", "POSMain: Initializing POSMain scene");
+
+        // Initialize UI elements
+        _exitButton = GetNode<Button>("%ExitButton");
+
+        // Connect button signals
+        _exitButton.Pressed += OnExitButtonPressed;
+    }
+
+    private void OnExitButtonPressed()
+    {
+        GetTree().CallDeferred("change_scene_to_file", "res://Scenes/Home.tscn");
     }
 
 }
