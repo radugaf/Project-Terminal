ba789440002ac723eff55eb35686937f513b8078
Radu Gafita <radugaf@gmail.com>
2025-03-28 21:34:14 +0200

    Added email authentication.

---
+++ Globals/CategoryManager.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+using ProjectTerminal.Resources;
+using Supabase.Postgrest.Responses;
+using Supabase.Postgrest;
+
+public partial class CategoryManager : Node
+{
+    private Node _logger;
+    private SupabaseClient _supabaseClient;
+    private OrganizationManager _organizationManager;
+
+    [Signal]
+    public delegate void CategoryCreatedEventHandler(string addressId);
+
+    public override void _Ready()
+    {
+        _logger = GetNode<Node>("/root/Logger");
+        _logger.Call("info", "AddressManager: Initializing");
+
+        _supabaseClient = GetNode<SupabaseClient>("/root/SupabaseClient");
+        _organizationManager = GetNode<OrganizationManager>("/root/OrganizationManager");
+    }
+
+    public async Task<string> CreateCategoryAsync(string name, string icon, string color)
+    {
+        try
+        {
+            var category = new Category
+            {
+                Name = name,
+                Icon = icon,
+                Color = color,
+                OrganizationId = _organizationManager.GetOrganizationId()
+            };
+
+            ModeledResponse<Category> response = await _supabaseClient.From<Category>()
+                .Insert(category, new QueryOptions { Returning = QueryOptions.ReturnType.Representation });
+
+            if (response == null || response.ResponseMessage.IsSuccessStatusCode != true)
+            {
+                _logger.Call("error", $"CategoryManager: Failed to create category: {response?.ResponseMessage.ReasonPhrase}");
+                throw new Exception($"Failed to create category");
+            }
+
+            string categoryId = response.Model?.Id;
+            _logger.Call("info", $"CategoryManager: Category created with ID: {categoryId}");
+
+            EmitSignal(SignalName.CategoryCreated, categoryId);
+            return categoryId;
+        }
+        catch (Exception ex)
+        {
+            _logger.Call("error", $"CategoryManager: Category creation failed: {ex.Message}");
+            throw;
+        }
+    }
+}
